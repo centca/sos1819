059322f8e26a6cc6289544ee202cd1108b1c921a
sos1819jmcc <jmcentenocaballero@gmail.com>
2019-03-23 02:14:25 +0000

    create mongoDB

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb+srv://test:test@sos-nwznc.mongodb.net/sos?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var contacts;
+
+client.connect(err => {
+  contacts = client.db("sos1819").collection("contacts");
+  console.log("Connected!");
+});
+
 var app = express();
 
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-app.post("/", (req, res) => {
-
-    console.log("Hola");
+app.get("/contacts", (req,res)=>{
+    
+    contacts.find({}).toArray((err,contactsArray)=>{
+        
+        if(err)
+            console.log("Error: "+err);
+        
+        res.send(contactsArray);        
+    });
 
 });
 
-//----------------------------------------------------------------------------
+
+// POST /contacts/
+
+app.post("/contacts", (req,res)=>{
+    
+    var newContact = req.body;
+    
+    contacts.insert(newContact);
+    
+    res.sendStatus(201);
+});
 
 
 app.listen(port, () => {
-
     console.log("Super server ready on port " + port);
 });
